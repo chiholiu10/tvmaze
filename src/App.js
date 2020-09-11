6e9286a3915929e1b19da35cd4dd6693cdc853aa
Chi Ho <>
2020-09-12 00:39:03 +0200

    redux and router installed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Route, Switch } from "react-router-dom";
+import { CatalogPage } from './components/CatalogPage/CatalogPage';
+// import EpisodePage from './components/EpisodePage/EpisodePage';
+// import "./styles.scss";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Switch>
+          <Route exact path="/" render={() => <CatalogPage />} />
+          {/* <Route path="/EpisodePage" render={() => <EpisodePage />} /> */}
+        </Switch>
+
     </div>
   );
 }
 
-export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import { catalogData } from './catalogData';
+
+export default combineReducers({
+    catalogData
+});
