2a95a83493939a9bbe8d95040bf92e644d2b49b0
Chi Ho <>
2020-09-13 22:04:05 +0200

    bracket import app component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
-import App from './App';
+import { App } from './App';
 import { Router } from "react-router-dom";
 import history from "./history";
 import * as serviceWorker from './serviceWorker';
