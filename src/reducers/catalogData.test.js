b465f6385fb6e196aaf486faffda5305997688ec
Chi Ho <>
2020-09-14 22:15:36 +0200

    fix linter

--- src/reducers/catalogData.test.js
+++ src/reducers/catalogData.test.js
@@ -10,31 +10,31 @@ test('return reducer default values', () => {
 });
 
 test('test get data reducer', () => {
-    const action = {
-        type: types.GET_DATA,
-        data: 'test'
-    }
-    expect(catalogData({}, action)).toEqual({
-        catalogInfo: 'test'
-    })
+	const action = {
+		type: types.GET_DATA,
+		data: 'test'
+	};
+	expect(catalogData({}, action)).toEqual({
+		catalogInfo: 'test'
+	});
 });
 
 test('test get data reducer', () => {
-    const action = {
-        type: types.GET_EPISODE,
-        list: fixedApi.fixedApi
-    }
-    expect(catalogData({}, action)).toEqual({
-        episdeList: fixedApi.fixedApi
-    })
+	const action = {
+		type: types.GET_EPISODE,
+		list: fixedApi.fixedApi
+	};
+	expect(catalogData({}, action)).toEqual({
+		episdeList: fixedApi.fixedApi
+	});
 });
 
 test('test get index reducer', () => {
-    const action = {
-        type: types.GET_INDEX,
-        index: 1
-    }
-    expect(catalogData({}, action)).toEqual({
-        currentIndex: 1
-    })
+	const action = {
+		type: types.GET_INDEX,
+		index: 1
+	};
+	expect(catalogData({}, action)).toEqual({
+		currentIndex: 1
+	});
 });
