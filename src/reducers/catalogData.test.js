2c2c349f255063cd64e00ace02b3fafa35e67b24
Chi Ho <>
2020-09-14 19:20:58 +0200

    added reducer testing

--- src/reducers/catalogData.test.js
+++ src/reducers/catalogData.test.js
@@ -1,8 +1,29 @@
 import { catalogData } from './catalogData';
+import { types } from '../actions/index';
 
 test('return reducer default values', () => {
 	expect(catalogData(undefined, {})).toEqual({
 		catalogInfo: [],
 		episodeList: []
 	});
+});
+
+test('test get data reducer', () => {
+    const action = {
+        type: types.GET_DATA,
+        data: 'test'
+    }
+    expect(catalogData({}, action)).toEqual({
+        catalogInfo: 'test'
+    })
+});
+
+test('test get index reducer', () => {
+    const action = {
+        type: types.GET_INDEX,
+        index: 1
+    }
+    expect(catalogData({}, action)).toEqual({
+        currentIndex: 1
+    })
 });
